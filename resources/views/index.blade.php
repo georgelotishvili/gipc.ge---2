cec994be68d37371ca519d499319785b92039fa8
NikolozXN <131599542+NikolozXN@users.noreply.github.com>
2024-09-10 12:04:23 +0400

    Feat:Add Views

---
+++ resources/views/index.blade.php
@@ -0,0 +1,80 @@
+<x-layout>
+    <!-- ====== Hero Section Start -->
+    <div id="home" class="relative overflow-hidden bg-primary pt-[120px] md:pt-[130px] lg:pt-[160px]">
+        <div class="container">
+            <div class="-mx-4 flex flex-wrap items-center">
+                <div class="w-full px-4">
+                    <div class="hero-content wow fadeInUp mx-auto max-w-[780px] text-center" data-wow-delay=".2s">
+                        <h1
+                            class="mb-6 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]">
+                            <span class="text-7xl font-extrabold">GIPC</span>, თქვენი
+                            პარტნიორი პროფესიული განვითარების გზაზე
+                        </h1>
+                        <p
+                            class="mx-auto mb-9 max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]">
+                            საქართველოს პროფესიული სერთიფიცირების ინსტიტუტი (GIPC)
+                        </p>
+                    </div>
+                    <ul class="mb-8 flex flex-wrap items-center justify-center gap-5 hero-content wow fadeInUp"
+                        data-wow-delay=".1.5s">
+                        <li>
+                            <a href=""
+                                class="inline-flex items-center justify-center rounded-md bg-white bg-opacity-20 px-4 py-[10px] text-center text-base font-medium text-white shadow-1 transition duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">
+                                ინსტიტუტის შესახებ
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""
+                                class="inline-flex items-center justify-center rounded-md bg-white bg-opacity-20 text-w px-4 py-[10px] text-center text-base font-medium text-white shadow-1 transition duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">
+                                ინსტიტუტის დებულება
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""
+                                class="inline-flex items-center justify-center rounded-md bg-white bg-opacity-20 text-w px-4 py-[10px] text-center text-base font-medium text-white shadow-1 transition duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">
+                                დადებითი გავლენა
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""
+                                class="inline-flex items-center justify-center rounded-md bg-white bg-opacity-20 text-w px-4 py-[10px] text-center text-base font-medium text-white shadow-1 transition duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">
+                                134 დადგენილება
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""
+                                class="inline-flex items-center justify-center rounded-md bg-white bg-opacity-20 text-w px-4 py-[10px] text-center text-base font-medium text-white shadow-1 transition duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark">
+                                წესები და პირობები
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="flex justify-start flex-col mt-5 gap-2 p-6 hero-content wow fadeInUp"
+                        data-wow-delay=".1.5s">
+                        <h1 class="text-white text-4xl mb-1">
+                            საქართველოს პროფესიული სერთიფიცირების ინსტიტუტი (GIPC)
+                        </h1>
+                        <p class="text-white text-xl mb-2">მისია და მიზნები</p>
+                        <p class="text-white text-md mb-10">
+                            საქართველოს პროფესიული სერთიფიცირების ინსტიტუტი (GIPC) დაარსდა
+                            საქართველოს მთარობის დადგენილების &numero;134 (2024 წლის 10
+                            აპრილი) შესაბამისად, არქიტექტორებისა და ინჟინერ-მშენებლების
+                            სერთიფიცირების სისტემის მართვის მიზნით. ჩვენი ინსტიტუტის მთავარი
+                            მიზანია, უზრუნველვყოთ სამშენებლო სექტორში მოღვაწე
+                            პროფესიონალების მაღალი კვალიფიკაცია და პროფესიული სტანდარტების
+                            დაცვა. საქართველოს პროფესიული სერთიფიცირების ინსტიტუტი (GIPC)
+                            დაარსდა საქართველოს მთარობის დადგენილების &numero;134 (2024 წლის
+                            10 აპრილი) შესაბამისად, არქიტექტორებისა და ინჟინერ-მშენებლების
+                            სერთიფიცირების სისტემის მართვის მიზნით. ჩვენი ინსტიტუტის მთავარი
+                            მიზანია, უზრუნველვყოთ სამშენებლო სექტორში მოღვაწე
+                            პროფესიონალების მაღალი კვალიფიკაცია და პროფესიული სტანდარტების
+                            დაცვა.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="w-full px-4"></div>
+            </div>
+        </div>
+    </div>
+    <!-- ====== Hero Section End -->
+</x-layout>
---
+++ resources/views/questions.blade.php
@@ -0,0 +1,81 @@
+<x-layout>
+    <!-- ====== Questions Section Start -->
+    <div class="relative z-10 overflow-hidden pb-[60px] pt-[120px] dark:bg-dark md:pt-[130px] lg:pt-[160px]">
+        <h1
+            class="mb-4 text-2xl text-center font-bold text-dark dark:text-white sm:text-xl md:text-[24px] md:leading-[1.2]">
+            რატომ არის ჯავახეთის სოფელ აიაზმიში უკეთესი ასფალტი ვიდრე ნახალოვკაში?
+        </h1>
+        <p class="timer text-center mb-4 text-lg"></p>
+        <div class="container flex justify-center">
+            <div class="-mx-4">
+                <div class="px-4 flex flex-col gap-4 justify-center">
+                    <button
+                        class="wrong-answer  font-semibold py-2 px-4 border border-gray-300 rounded shadow-lg text-left max-w-md">
+                        რატომ არის ჯავახეთის სოფელ აიაზმიში უკეთესი ასფალტი ვიდრე
+                        ნახალოვკაში რატომ არის ჯავახეთის სოფელ აიაზმიში უკეთესი ასფალტი
+                        ვიდრე ნახალოვკაში
+                    </button>
+                    <button
+                        class="answer  font-semibold py-2 px-4 border border-gray-300 rounded shadow-lg text-left max-w-md">
+                        რატომ არის ჯავახეთის სოფელ აიაზმიში უკეთესი ასფალტი ვიდრე
+                        ნახალოვკაში
+                    </button>
+                    <button class=" font-semibold py-2 px-4 border border-gray-300 rounded shadow-md text-left max-w-md">
+                        რატომ არის ჯავახეთის სოფელ აიაზმიში უკეთესი ასფალტი ვიდრე
+                        ნახალოვკაში
+                    </button>
+                    <button class=" font-semibold py-2 px-4 border border-gray-300 rounded shadow-md text-left max-w-md">
+                        რატომ არის ჯავახეთის სოფელ აიაზმიში უკეთესი ასფალტი ვიდრე
+                        ნახალოვკაში
+                    </button>
+                </div>
+                <div class="flex justify-between items-center gap-4 px-2">
+                    <button type="button"
+                        class="text-white mt-4 ml-1 bg-primary hover:bg-blue-dark focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <i class="fa-solid fa-chevron-left fa-lg"></i>
+                    </button>
+                    <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                        <div class="bg-primary z-50 h-2.5 rounded-full dark:bg-primary" style="width: 75%"></div>
+                    </div>
+                    <button type="button"
+                        class="text-white mt-4 bg-primary hover:bg-blue-dark focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <i class="fa-solid fa-chevron-right fa-lg"></i>
+                    </button>
+                </div>
+                <span class="absolute top-0">23:14</span>
+            </div>
+        </div>
+    </div>
+    <!-- ====== Questions Section End -->
+</x-layout>
+<script>
+    let wrongAnswer = document.querySelector(".wrong-answer");
+    let answer = document.querySelector(".answer");
+    // let timer = document.querySelector(".timer");
+    wrongAnswer.addEventListener("click", function() {
+        wrongAnswer.classList.add("bg-red-500", "text-white");
+        answer.classList.add("bg-emerald-500", "text-white");
+    });
+
+    answer.addEventListener("click", function() {
+        answer.classList.add("bg-emerald-500", "text-white");
+    });
+
+
+    let secs = 1800;
+    let minutes = Math.floor(secs / 60);
+    let countDownSecs = secs % 60;
+
+    const timer = setInterval(() => {
+        document.querySelector(".timer").textContent =
+            `${minutes}:${countDownSecs.toString().padStart(2, '0')}`;
+
+        secs--;
+        minutes = Math.floor(secs / 60);
+        countDownSecs = secs % 60;
+
+        if (secs <= 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+</script>
