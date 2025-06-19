791e4ca79fb465f52d7414ff8bf8bcc7818f5fd3
SpaxXeN <keinashvilis@gmail.com>
2025-06-19 20:08:10 +0400

    Fix subscriptions and added UserSubscriptionSeeder

--- app/Livewire/UserSubscriptionModal.php
+++ app/Livewire/UserSubscriptionModal.php
@@ -3,8 +3,12 @@
 namespace App\Livewire;
 
 use App\Actions\Abecert\UserSanitizerAction;
+use App\Models\Plan;
+use App\Models\PlanType;
 use App\Models\Subscription;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\View\View;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
@@ -31,26 +35,32 @@ class UserSubscriptionModal extends Component
     {
         $this->user = User::find($userId);
         $this->subscription = $this->user->subscription;
-        $this->is_active = $this->subscription->is_active;
-        $this->type = $this->subscription->type;
-        $this->starts_at = $this->subscription->starts_at;
-        $this->ends_at = $this->subscription->ends_at;
+        $this->is_active = $this->user->hasActiveSubscription() ?: false;
+        $this->type = $this->user->subscription?->plan_id ?: 0;
+        $this->starts_at = $this->user->subscription?->starts_at ? Carbon::parse($this->user->subscription?->starts_at)->format('Y-m-d') : null;;
+        $this->ends_at = $this->user->subscription?->ends_at ? Carbon::parse($this->user->subscription?->ends_at)->format('Y-m-d') : null;
     }
 
     public function save()
     {
-        $this->subscription->is_active = $this->is_active;
-        $this->subscription->type = $this->type;
-        $this->subscription->starts_at = $this->starts_at ?: null;
-        $this->subscription->ends_at = $this->ends_at ?: null;
-        $this->subscription->save();
-        $this->user = User::find($this->user->id);
-        UserSanitizerAction::sanitize($this->user);
+        $plan = Plan::find($this->type);
+        $planType = PlanType::find($plan->plan_type_id);
+        $data = [
+            'plan_type_id' => $planType->id,
+            'plan_id' => $plan->id,
+            'is_active' => $this->is_active,
+            'starts_at' => Carbon::parse($this->starts_at)->startOfDay(),
+            'ends_at' => $this->ends_at != null ? Carbon::parse($this->ends_at)->endOfDay() : Carbon::now()->addDays($planType->type_duration),
+            'recToken' => md5(time()),
+        ];
+        $this->user->createOrUpdateSubscription($data);
         $this->dispatch('close-modal');
     }
 
-    public function render()
+    public function render(): View
     {
-        return view('livewire.user-subscription-modal');
+        return view('livewire.user-subscription-modal', [
+            'plans' => Plan::get()
+        ]);
     }
 }
