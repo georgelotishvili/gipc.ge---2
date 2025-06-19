aaa3175ce976d30feef8b11bfcf01e20fdd79ee7
SpaxXeN <keinashvilis@gmail.com>
2025-06-19 20:12:05 +0400

    Fix subscriptions and added UserSubscriptionSeeder

--- database/seeders/UserSubscriptionSeeder.php
+++ database/seeders/UserSubscriptionSeeder.php
@@ -16,73 +16,88 @@ class UserSubscriptionSeeder extends Seeder
     {
         $plan = Plan::find(3);
         $user = User::find(1);
-        $user->subscription()->updateOrCreate(['user_id' => $user->id],[
-            'plan_type_id' => $plan->plan_type_id,
-            'plan_id' => $plan->id,
-            'is_active' => true,
-            'starts_at' => Carbon::now(),
-            'ends_at' => Carbon::now()->addDays(1095),
-            'recToken' => md5(time().$user->id),
-        ]);
+
+        if($user != null){
+            $user->subscription()->updateOrCreate(['user_id' => $user->id],[
+                'plan_type_id' => $plan->plan_type_id,
+                'plan_id' => $plan->id,
+                'is_active' => true,
+                'starts_at' => Carbon::now(),
+                'ends_at' => Carbon::now()->addDays(1095),
+                'recToken' => md5(time().$user->id),
+            ]);
+        }
         $user = User::find(2);
         $plan = Plan::find(3);
-        $user->subscription()->updateOrCreate(['user_id' => $user->id],[
-            'plan_type_id' => $plan->plan_type_id,
-            'plan_id' => $plan->id,
-            'is_active' => true,
-            'starts_at' => Carbon::now(),
-            'ends_at' => Carbon::now()->addDays(1095),
-            'recToken' => md5(time().$user->id),
-        ]);
+        if($user != null){
+            $user->subscription()->updateOrCreate(['user_id' => $user->id],[
+                'plan_type_id' => $plan->plan_type_id,
+                'plan_id' => $plan->id,
+                'is_active' => true,
+                'starts_at' => Carbon::now(),
+                'ends_at' => Carbon::now()->addDays(1095),
+                'recToken' => md5(time().$user->id),
+            ]);
+        }
         $user = User::find(3);
         $plan = Plan::find(3);
-        $user->subscription()->updateOrCreate(['user_id' => $user->id],[
-            'plan_type_id' => $plan->plan_type_id,
-            'plan_id' => $plan->id,
-            'is_active' => true,
-            'starts_at' => Carbon::now(),
-            'ends_at' => Carbon::now()->addDays(1095),
-            'recToken' => md5(time().$user->id),
-        ]);
+        if($user != null){
+            $user->subscription()->updateOrCreate(['user_id' => $user->id],[
+                'plan_type_id' => $plan->plan_type_id,
+                'plan_id' => $plan->id,
+                'is_active' => true,
+                'starts_at' => Carbon::now(),
+                'ends_at' => Carbon::now()->addDays(1095),
+                'recToken' => md5(time().$user->id),
+            ]);
+        }
         $user = User::find(4);
         $plan = Plan::find(3);
-        $user->subscription()->updateOrCreate(['user_id' => $user->id],[
-            'plan_type_id' => $plan->plan_type_id,
-            'plan_id' => $plan->id,
-            'is_active' => true,
-            'starts_at' => Carbon::now(),
-            'ends_at' => Carbon::now()->addDays(1095),
-            'recToken' => md5(time().$user->id),
-        ]);
+        if($user != null){
+            $user->subscription()->updateOrCreate(['user_id' => $user->id],[
+                'plan_type_id' => $plan->plan_type_id,
+                'plan_id' => $plan->id,
+                'is_active' => true,
+                'starts_at' => Carbon::now(),
+                'ends_at' => Carbon::now()->addDays(1095),
+                'recToken' => md5(time().$user->id),
+            ]);
+        }
         $user = User::find(5);
         $plan = Plan::find(3);
-        $user->subscription()->updateOrCreate(['user_id' => $user->id],[
-            'plan_type_id' => $plan->plan_type_id,
-            'plan_id' => $plan->id,
-            'is_active' => true,
-            'starts_at' => Carbon::now(),
-            'ends_at' => Carbon::now()->addDays(1095),
-            'recToken' => md5(time().$user->id),
-        ]);
+        if($user != null){
+            $user->subscription()->updateOrCreate(['user_id' => $user->id],[
+                'plan_type_id' => $plan->plan_type_id,
+                'plan_id' => $plan->id,
+                'is_active' => true,
+                'starts_at' => Carbon::now(),
+                'ends_at' => Carbon::now()->addDays(1095),
+                'recToken' => md5(time().$user->id),
+            ]);
+        }
         $user = User::find(6);
         $plan = Plan::find(3);
-        $user->subscription()->updateOrCreate(['user_id' => $user->id],[
-            'plan_type_id' => $plan->plan_type_id,
-            'plan_id' => $plan->id,
-            'is_active' => true,
-            'starts_at' => Carbon::now(),
-            'ends_at' => Carbon::now()->addDays(1095),
-            'recToken' => md5(time().$user->id),
-        ]);
+        if($user != null){
+            $user->subscription()->updateOrCreate(['user_id' => $user->id],[
+                'plan_type_id' => $plan->plan_type_id,
+                'plan_id' => $plan->id,
+                'is_active' => true,
+                'starts_at' => Carbon::now(),
+                'ends_at' => Carbon::now()->addDays(1095),
+                'recToken' => md5(time().$user->id),
+            ]);
+        }
         $user = User::find(7);
         $plan = Plan::find(3);
-        $user->subscription()->updateOrCreate(['user_id' => $user->id],[
-            'plan_type_id' => $plan->plan_type_id,
-            'plan_id' => $plan->id,
-            'is_active' => true,
-            'starts_at' => Carbon::now(),
-            'ends_at' => Carbon::now()->addDays(1095),
-            'recToken' => md5(time().$user->id),
-        ]);
+        if($user != null){
+            $user->subscription()->updateOrCreate(['user_id' => $user->id],[
+                'plan_type_id' => $plan->plan_type_id,
+                'plan_id' => $plan->id,
+                'is_active' => true,
+                'starts_at' => Carbon::now(),
+                'ends_at' => Carbon::now()->addDays(1095),
+                'recToken' => md5(time().$user->id),
+            ]);
+        }
     }
 }
